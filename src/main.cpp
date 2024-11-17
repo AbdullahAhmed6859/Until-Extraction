7b8f52981041bc10ffdf2da85a603115fb006b8f
AbdullahAhmed6859 <abdullahahmed2004@gmail.com>
2024-11-17 23:48:15 +0500

    assets added

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-  auto window = sf::RenderWindow({1000u, 1000u}, "CMake SFML Project");
-  window.setFramerateLimit(144);
+    auto window = sf::RenderWindow({1000u, 1000u}, "CMake SFML Project");
+    window.setFramerateLimit(144);
 
-  while (window.isOpen()) {
-    for (auto event = sf::Event(); window.pollEvent(event);) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
+    while (window.isOpen()) {
+        for (auto event = sf::Event(); window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        window.display();
     }
-
-    window.clear();
-    window.display();
-  }
 }
