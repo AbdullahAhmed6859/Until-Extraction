a08cbb5e1dc05456ca0d98de78fcb0e89bb87fca
AbdullahAhmed6859 <abdullahahmed2004@gmail.com>
2024-12-03 22:47:58 +0500

    Project submission

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    Game game("Until Rescue");
-    // Game game(1280, 720,"Until Rescue");
+    // Game game("Until Rescue");
+    Game game(1280, 720,"Until Rescue");
     game.run();
     return 0;
 }
